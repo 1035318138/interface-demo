011542f5b10bfcef7dda959dc9e6f7d5ed3d8897
Can.Ru <8016667hao000>
2022-07-18 15:46:38 +0800

    init

---
+++ src/main/java/com/nkznb/interfacedemo/InterfaceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nkznb.interfacedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nkznb/interfacedemo/InterfaceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkznb.interfacedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
